0532f8e0d8baea32fd7d467761ae30ae5a5aaa19
alef <andreev.alef@gmail.com>
2021-10-12 14:19:44 +0300

    Автогенерация пользователя moodle

---
+++ index1.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <style>
+            body{
+                /*font-family: sans-serif;*/
+            }
+
+        </style>    
+    </head>
+    <body>
+        <?php
+        ini_set('error_reporting', E_ALL);
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+
+        function getRandomPassword($passLen = 8) {
+            $symbols = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&?';
+            $pass = '';
+            $alphaLength = strlen($symbols) - 1;
+            for ($i = 0; $i < $passLen; $i++) {
+                $n = rand(0, $alphaLength);
+                $pass = $pass . $symbols[$n];
+            }
+            return $pass;
+        }
+
+        $pwd = getRandomPassword(12);
+        ?>
+        <div style="font-family: 'Courier New'; font-size: 14pt; text-align: center">
+            <tt><?= $pwd ?></tt>
+        </div>
+    </body>
+</html>
